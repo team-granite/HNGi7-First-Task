2c04eb1004ad999da1d4081f414bbbd69feba47c
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-06-03 21:00:38 +0000

    Update samuelorobosa.js

--- scripts/samuelorobosa.js
+++ scripts/samuelorobosa.js
@@ -1,5 +1,5 @@
 
-let output = "Hello World, this is Samuel Amagbakhen with HNGi7 ID HNG-00558 using Javascript for stage 2 task and amagbakhensamuel@gmail.com"
+let output = "Hello World, this is Samuel Amagbakhen with HNGi7 ID HNG-00558 using JavaScript for stage 2 task and amagbakhensamuel@gmail.com"
 
 let myFunction=()=>{
 console.log(output);
