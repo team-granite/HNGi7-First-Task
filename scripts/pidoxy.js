a2461ac3022d79822ecfd7140a3e9ec2097b99f5
pidoxy <56538561+pidoxy@users.noreply.github.com>
2020-06-03 13:57:31 +0100

    still testing

--- scripts/pidoxy.js
+++ scripts/pidoxy.js
@@ -6,9 +6,8 @@ let intern_pidoxy = {
 }
 
 function giveOutput() {
-    let output = `Hello World, this is ${intern_pidoxy.fullName} with HNGi7 ID ${intern_pidoxy.ID} using ${intern_pidoxy.language} for stage 2 task`
-    console.log(output);
-    return output;
+    console.log(`Hello World, this is ${intern_pidoxy.fullName} with HNGi7 ID ${intern_pidoxy.ID} using ${intern_pidoxy.language} for stage 2 task`);
+
 }
 
 giveOutput();
