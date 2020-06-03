2f89ee21298f5a1d55cf91b6169908ce99c10193
cre8tcodes <baolinnovationz@gmail.com>
2020-06-03 19:19:12 +0100

    Add first task file using JavaScript

---
+++ scripts/Cre8tcodes-04384.js
@@ -0,0 +1,11 @@
+const name = 'Babawale Olojo';
+
+const email = 'baolinnovationz@gmail.com';
+
+const id = 'HNG-04384';
+
+const lang = 'Javascript';
+
+let result = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task, my email is ${email}.` ;
+
+console.log(result)
