5a81c0d605530ea779d5875e30fb42de2ae688f5
jonyoung123 <64228597+jonyoung123@users.noreply.github.com>
2020-06-03 15:47:57 +0100

    Add files via upload

---
+++ scripts/john.py.py
@@ -0,0 +1,4 @@
+def my_script():
+    my_details= {"fullname": "Eke John Igwe", "ID": "HNG-04023", "language": "Python", "email": "stephenjohn5646@gmail.com"}
+    print("Hello World, this is " +my_details["fullname"]+ " with HNGi7 ID " +my_details["ID"] + " using " +my_details["language"]+ " for stage 2 task and " + my_details["email"])
+my_script()
