46f6ffea95e740c9b42e0529adba7f80c367d0c1
pidoxy <56538561+pidoxy@users.noreply.github.com>
2020-06-03 15:23:54 +0100

    more fix

--- scripts/pidoxy.js
+++ scripts/pidoxy.js
@@ -5,8 +5,9 @@ let intern_pidoxy = {
     email: "pidox31102002@gmail.com"
 }
 
+let output = `Hello World, this is ${intern_pidoxy.fullName} with HNGi7 ID ${intern_pidoxy.ID} using ${intern_pidoxy.language} for stage 2 task`);
+
 function giveOutput() {
-    let output = `Hello World, this is ${intern_pidoxy.fullName} with HNGi7 ID ${intern_pidoxy.ID} using ${intern_pidoxy.language} for stage 2 task`);
     console.log(output);
     
 
