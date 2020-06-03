76f872f78d86ea95b1d23e4fd3bcc4bb12c0cc36
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-06-03 14:45:57 +0100

    new logic

--- scripts/joshdev.js
+++ scripts/joshdev.js
@@ -1,9 +1,8 @@
-let content = "Hello World, this is Joshua Adesanya with HNGi7 ID HNG-02333 using javascript for stage 2 task"
-let email = "adesanyajoshua@ymail.com";
+let content = "Hello World, this is Joshua Adesanya with HNGi7 ID HNG-02333 using javascript for stage 2 task and adesanyajoshua@ymail.com"
+
 
 const joshscript = () => {
     console.log(content);
-    console.log(email);
 }
 
 joshscript();
