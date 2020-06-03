fa4d8fa3cf07879217a20e1707b4654684669c96
Joshua Odejide <54546188+joshua-s2@users.noreply.github.com>
2020-06-03 14:24:45 +0100

    Create Josh-Dev.js

---
+++ scripts/Josh-Dev.js
@@ -0,0 +1,5 @@
+var intern = 'Hello World, this is Joshua Jesutobi Odejide with HNGi7 ID HNG-02275 using Javascript for stage 2 task'
+var email ="joshuaodejide@gmail.com"
+console.log(intern);
+console.log(email);
+
