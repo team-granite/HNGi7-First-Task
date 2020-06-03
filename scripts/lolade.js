685501805075030183a7de995704a03e7ee3e5b0
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2020-06-03 20:54:03 +0100

    modified

--- scripts/lolade.js
+++ scripts/lolade.js
@@ -4,6 +4,6 @@
     let language="javascript";
     let email="lodfreelance@gmail.com"
 
-    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task and  ${email}`);
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task and ${email}`);
 
 })();
