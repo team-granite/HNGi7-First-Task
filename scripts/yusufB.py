c284db16986c85be87feaaa1c4a62dd5ceb1436b
Yusuf-AH <66280341+Yusuf-AH@users.noreply.github.com>
2020-06-03 15:59:06 +0100

    Add files via upload

---
+++ scripts/yusufB.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+
+fname = "Yusuf Braimoh"
+HNGi7_ID = "HNG-05068"
+language = "Python"
+email = "braimohyusuf@gmail.com"
+
+print(f"Hello World, this is {fname} with HNGi7 ID {HNGi7_ID} and email {email} using {language} for stage 2 task. ")
