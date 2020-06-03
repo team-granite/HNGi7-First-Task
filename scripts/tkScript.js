245229247c9fea12f0baf13713f1a3fcfd5299f2
tochukwu chinedu <tochukwuchinedu21@gmail.com>
2020-06-03 15:39:35 +0100

    update

--- scripts/tkScript.js
+++ scripts/tkScript.js
@@ -11,13 +11,8 @@ let details = {
     email
 }
 
-const outputFunction = ({fullName, HNGID, language}) => {
-    console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + HNGID + ' using ' + language + ' for stage 2 task')
-}
-
-const outputEmail = ({email}) => {
-    console.log(email)
+const outputFunction = ({fullName, HNGID, language,email}) => {
+    console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + HNGID + ' using ' + language + ' for stage 2 task and ' + email)
 }
 
 outputFunction(details)
-outputEmail(details)
