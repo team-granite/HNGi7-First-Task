768c96f9b1ea086629b42b5a3b401789709b4e39
gabbie amah <gabbieamah49@gmail.com>
2020-06-03 17:22:51 +0100

    Gabriells js

---
+++ scripts/GabriellaAmah.js
@@ -0,0 +1,10 @@
+
+let   fullName =  'Gabriella Amaefule';
+let  ID =  'HNG-02535';
+let  language =  'javascript';
+    
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2`)
+    
+    
+
