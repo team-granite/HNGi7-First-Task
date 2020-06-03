5c9771f5df5f6316c48671f00ffdff961241a482
Ahmed Mansour <moodfactor@hotmail.com>
2020-06-03 19:58:11 +0200

    mood-HNG-01489 script

---
+++ scripts/mood-HNG-01489.js
@@ -0,0 +1,14 @@
+
+//defining the output
+let output = "Hello World, this is Ahmed Mansour with HNGi7 ID HNG-01489 using javascript for stage 2 task and email moodfactor@gmail.com";
+
+
+//defining the function to out
+function outputScript() {
+
+    //logging the outpu 
+    console.log(output);
+}
+
+//calling the function creatd
+outputScript();
