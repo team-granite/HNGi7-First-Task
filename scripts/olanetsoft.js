007a4e2380492c2c182af9d8c2af2fe9a84219e6
olanetsoft <heedris2olubisi@gmail.com>
2020-06-03 03:41:03 +0100

    INCLUDED OLANETSOFT.JS IN THE SCRIPTS FOLDER

---
+++ scripts/olanetsoft.js
@@ -0,0 +1,20 @@
+//variable declaration
+var fullName = "Idris Olubisi";
+var ID = "HNG-01329";
+var language = "javascript"
+
+
+//defining the output
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+
+
+//defining the function to ou
+function outputScript() {
+
+    //logging the output
+    console.log(output);
+}
+
+//calling the function created
+outputScript();
+
