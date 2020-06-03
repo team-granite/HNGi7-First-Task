6c11fd40e2ae9aa904b448e60ea43dc1d7aedee1
tochukwu chinedu <tochukwuchinedu21@gmail.com>
2020-06-03 07:46:58 +0100

    refactored

--- scripts/tkScript.js
+++ scripts/tkScript.js
@@ -3,4 +3,14 @@ let fullName = 'Nwankwu Tochukwu'
 let HNGID = 'HNG-02704'
 let language = 'javascript'
 
-console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + HNGID + ' using ' + language + ' for stage 2 task')
+let details = {
+    fullName,
+    HNGID,
+    language
+}
+
+const outputFunction = ({fullName, HNGID, language}) => {
+    console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + HNGID + ' using ' + language + ' for stage 2 task')
+}
+
+outputFunction(details)
