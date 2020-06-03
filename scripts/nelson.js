a8a25091566a90912977b7ead2e831ce6ac5f2b0
Nelson <egwuchinedu69@gmail.com>
2020-06-03 17:22:56 +0100

    add my script

---
+++ scripts/nelson.js
@@ -0,0 +1,9 @@
+let userDetail = {
+  firstName: "Nelson",
+  lastName: "Chinedu",
+  id: "HNG-04904",
+  language: "javaScript",
+  email: "egwuchinedu69@gmail.com"
+};
+let detail = 'Hello World, this is ' + userDetail["firstName"] + ' ' + userDetail["lastName"] + ' with HNGi7 ID ' + userDetail["id"] + ' using ' + userDetail["language"] + ' for stage 2 task and ' + userDetail["email"];
+console.log(detail);
