c080805564df69609baf572116346e2f20140473
Tunechi-chris <66219578+Tunechi-chris@users.noreply.github.com>
2020-06-03 11:10:35 -0700

    Update Tunechi.js

--- scripts/Tunechi.js
+++ scripts/Tunechi.js
@@ -1,9 +1,9 @@
 let person = {
    
    FullName:'Mudalue Emeka',
-    slack : 'HNG-01029',
+    HNGID : 'HNG-01029',
     language: 'javascript',
     email:'emekachristian511@gmail.com'
 };
 
-console.log(JSON.stringify(`Hello World, this is ${person.FullName} with HNGi7 ID ${person.slack} using ${person.language} for stage 2 task`));
+console.log(JSON.stringify(`Hello World, this is ${person.FullName} with HNGi7 ID ${person.HNGID} using ${person.language} for stage 2 task`));
