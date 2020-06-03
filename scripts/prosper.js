b61bfef650ae445eddedeba1c06d17f99c28b8a5
Prosper Ekwerike <43956851+Pekwerike@users.noreply.github.com>
2020-06-03 16:54:03 +0100

    Update prosper.js
    added email address

--- scripts/prosper.js
+++ scripts/prosper.js
@@ -2,7 +2,7 @@ const firstName = "Prosper";
 const lastName = "Ekwerike";
 const hngID = "HNG-05129";
 const language = "Javascript";
-
-let outputResult = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`;
+const emailAddress = "pekwerike@gmail.com"
+let outputResult = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task and ${emailAddress}`;
 
 console.log(outputResult);
