89657cd741a210885569e251380b77e338ad692d
GP <powerex4greatness@gmail.com>
2020-06-03 22:25:58 +0100

    test passed locally

--- scripts/gp.php
+++ scripts/gp.php
@@ -2,7 +2,7 @@
 
 $bio = [ "fullName" => "Godspower Peter", "hngID" => "HNG-03273", "language" => "PHP", "email" => "powerex4greatness@gmail.com" ];
 
-echo "Hello World, this is {$bio['fullName']} with HNGi7 ID {$bio['hngID']} using {$bio['language']} for stage 2 and {$bio['email']}"
+echo "Hello World, this is {$bio['fullName']} with HNGi7 ID {$bio['hngID']} using {$bio['language']} for stage 2 task and {$bio['email']}"
 
 ?>
 
