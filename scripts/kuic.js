4a073ac2e80fb3fd581d59cc24c4eea0b1c3fdaa
Kingsley <kingsleyumujeyan@gmail.com>
2020-06-03 20:15:23 +0100

    bg--typo fixed

--- scripts/kuic.js
+++ scripts/kuic.js
@@ -3,7 +3,7 @@ const hngId = "HNG-02606";
 const language = "JavaScript";
 const email = "kingsleyumujeyan@gmail.com";
 
-const myDetails = `Hello World,this is ${name} with HNGi& ID ${hngId} using ${language} for Stage 2 task and ${email}`;
+const myDetails = `Hello World,this is ${name} with HNGi7 ID ${hngId} using ${language} for Stage 2 task and ${email}`;
 
 console.log(myDetails);
 
