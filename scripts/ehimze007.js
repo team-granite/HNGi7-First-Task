667a5e2ee07fff7ab3bc78aadad3b10102af4dbf
ehimze007 <ehimze007@gmail.com>
2020-06-03 19:35:03 +0100

    #### Change to be committed:
     There is just one change I am committing and that is the addition of my
     script file.
    
     -new file:   scripts/ehimze007.js

---
+++ scripts/ehimze007.js
@@ -0,0 +1,13 @@
+//Declaration and Initialization of variables
+let myName = "Ehimze Nathanael";
+let myId = "HNG-05231";
+let myLanguage = "Javascript";
+let myEmail = "ehimze007@gmail.com";
+
+//function declaration
+function ehimzeIdentificationString(){
+  console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myLanguage} for stage 2 task and ${myEmail}`);
+}
+
+//my function call
+ehimzeIdentificationString();
