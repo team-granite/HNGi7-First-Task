263b10fff80e06287204f5308d66a931fb866f41
Collins Wilson <wilsoncollins44@yahoo.com>
2020-06-03 23:28:31 +0100

    added cre8ive_collins.py script

---
+++ scripts/cre8ive_collins.py
@@ -0,0 +1,10 @@
+#imports
+import sys
+import json
+
+
+info = "Hello World, this is Collins Wilson with HNGi7 ID HNG-04747 using Python for stage 2 task"
+email = "wilsoncollins44@yahoo.com"
+
+
+print(f"{info} and {email}")
