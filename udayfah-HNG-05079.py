7d298d42408186c3e6ca4ce5441c533e62f2f745
udayfah <63933925+udayfah@users.noreply.github.com>
2020-06-03 16:46:38 +0100

    Update udayfah-HNG-05079.py

--- udayfah-HNG-05079.py
+++ udayfah-HNG-05079.py
@@ -3,6 +3,6 @@ id = "HNG-05079"
 language = "python"
 email_address = "holaosebikan005@gmail.com"
 
-info = f"Hello World, this is {name} with HNGi7 {id} using {language} for stage 2 task and {email_address}"
+info = f"Hello World, this is {name} with HNGi7 {id} using {language} for stage 2 task"
 
 print(info)
