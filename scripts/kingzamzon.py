bcba6cd68da4e89bdf6c90410bfd4d979790c9f0
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-06-04 00:15:56 +0100

    Script file from Adesanoye Samson

---
+++ scripts/kingzamzon.py
@@ -0,0 +1,14 @@
+
+'''
+    @author: Adesanoye Samson
+'''
+
+data = {
+    "name": "Adesanoye Samson",
+    "hng_id": "HNG-04139",
+    "email": "adesanoye.samson1@gmail.com"
+}
+
+string = "Hello World, this is " + data['name'] + " with HNGi7 ID " + data['hng_id'] + " using Python for stage 2 task and " + data['email']
+
+print(string)
