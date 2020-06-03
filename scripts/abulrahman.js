cac59ee35b76e031b7b8c03e0d2d9766b2d65b84
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-06-03 06:21:21 +0000

    Update abulrahman.js

--- scripts/abulrahman.js
+++ scripts/abulrahman.js
@@ -1,6 +1,6 @@
 let name = "Yusuf Abdulrahman";
-let hngID = HNG-06182;
-let language = Javascript;
+let hngID = "HNG-06182";
+let language = "Javascript";
 
 let result = `Hello World, this is ${name} with HNGi7 ID ${hngID} using ${language} for stage 2 task`;
 
