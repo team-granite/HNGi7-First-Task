004f0102bcf2b1adc24efc2d3fd44c3108d2642d
Abisoyesam <abisoyesam@gmail.com>
2020-06-03 20:53:49 +0100

    adding my email

--- scripts/LOYE.js
+++ scripts/LOYE.js
@@ -4,6 +4,8 @@ const id = 'HNG-02078';
 
 const lang = 'Javascript';
 
-let result = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+const email = 'abisoyeakinloye@gmail.com'
+
+let result = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task and my email is ${email}`;
 
 console.log(result)
