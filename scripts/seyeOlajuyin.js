20825c2c21e572882f6faca4eb45b2312342718a
Seye Olajuyin <seyeolajuyinlive@gmail.com>
2020-06-03 17:03:48 +0100

    seyeOlajuyin Script

--- scripts/seyeOlajuyin.js
+++ scripts/seyeOlajuyin.js
@@ -1,2 +1,2 @@
-const seyeData = `Hello World, this is Seye Olajuyin with HNGi7 ID HNG-02053 using JavaScript for stage 2 task and seyeolajuyinlive@gmail.com`;
-console.log(seyeData);
+const seeData = `Hello World, this is Seye Olajuyin with HNGi7 ID HNG-02053 using JavaScript for stage 2 task and seyeolajuyinlive@gmail.com`;
+console.log(seeData);
