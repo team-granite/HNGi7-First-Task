0fa4c89c77908c175a4d80c83c0eaca347215036
shit2boy <writeshittu@gmail.com>
2020-06-03 17:17:52 +0100

    my scripts

--- scripts/writeshittu.js
+++ scripts/writeshittu.js
@@ -6,12 +6,8 @@ const Lang = "JavaScript";
 let myInfo = { name, HNGi7_Id, email, Lang };
 
 const outputData = ({ name, HNGi7_Id, email, Lang }) => {
-  console.log(`Hello World, this is ${name} with HNGi7 ID ${HNGi7_Id} using ${Lang} for stage 2 task,
-    Name : ${name},
-    id : ${HNGi7_Id},
-    email: ${email},
-    Language : ${Lang}
+  return (`Hello World, this is ${name} with HNGi7 ID ${HNGi7_Id} using ${Lang} for stage 2 task,and email: ${email}
    `);
 };
 
-outputData(myInfo);
+console.log(outputData());
