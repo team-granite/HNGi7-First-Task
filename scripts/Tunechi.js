46e87130951c508598876b68f205c6c187851cc9
Tunechi-chris <66219578+Tunechi-chris@users.noreply.github.com>
2020-06-03 11:07:34 -0700

    Add files via upload

---
+++ scripts/Tunechi.js
@@ -0,0 +1,9 @@
+let person = {
+   
+   FullName:'Mudalue Emeka',
+    slack : 'HNG-01029',
+    language: 'javascript',
+    email:'emekachristian511@gmail.com'
+};
+
+console.log(JSON.stringify(`Hello World, this is ${person.FullName} with HNGi7 ID ${person.slack} using ${person.language} for stage 2 task`));
