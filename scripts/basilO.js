d7cc76173739e2acce5184be6311b1e2c377c1c2
Basil <obasibas260@gmail.com>
2020-06-03 15:41:49 -0400

    my script commit to the scripts directory

---
+++ scripts/basilO.js
@@ -0,0 +1,8 @@
+let info = {
+    fullName: "Obasi Basil",
+    id: "HNG-02505",
+    language: "JavaScript",
+    email: "obasibas260@gmail.com"
+};
+
+console.log(`Hello World, this is ${info.fullName} with HNGi7 ID ${info.id} using ${info.language} for stage 2 task`);
