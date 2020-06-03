02015903c742df27895e0911d5b57078801dfb31
lawal bolaji <lawalabdulrasheedbolaji@gmail.com>
2020-06-03 17:45:38 +0100

    Custom script created and tested

---
+++ scripts/abdulrasheed.lawal-HNG-03240.py
@@ -0,0 +1,9 @@
+details = {
+    "name" : "Abdulrasheed Lawal",
+    "HNG ID" : "HNG-03240"
+}
+
+def first_task():
+    return "Hello World, this is {} with HNG ID {} using python for stage 2 task".format(details["name"], details["HNG ID"])
+
+print(first_task())
