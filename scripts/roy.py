8fb16b40c3b89a5aff19ee35d00381145aae5ed5
roy98 <roy.biaga@pethersolutions.com>
2020-06-03 12:05:05 +0100

    Add my own script writed in Python

---
+++ scripts/roy.py
@@ -0,0 +1,9 @@
+person = {
+		'full_name': 'NDJANFA BIAGA Roy Christo',
+		'id'	   : 'HNG-05546',
+		'language' : 'Python',
+	}
+success = 'Hello World, this is {} with HNGi7 ID: [{}] using language: {} for stage 2 task'.format(
+	   person['full_name'], person['id'], person['language'])
+print(success)
+		
