a4a65898fd21f642e24c06cd97b7a4959f5fd4d6
teezamani <tee4tayour@gmail.com>
2020-06-03 16:07:23 +0100

    teezamani file

---
+++ scripts/teezamani.js
@@ -0,0 +1,7 @@
+//Var declaration
+var fullName = "Adepoju Ahmad-Tijani";
+var id ="HNG-04962";
+var email= "teezamani@gmail.com";
+var language = "C-Sharp"
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
