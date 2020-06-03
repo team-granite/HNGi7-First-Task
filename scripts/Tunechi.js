eee04a759a24ea58d4d5094dd2392f85c240093f
Tunechi-chris <66219578+Tunechi-chris@users.noreply.github.com>
2020-06-03 13:51:10 -0700

    Delete Tunechi.js

--- scripts/Tunechi.js
+++
@@ -1,9 +0,0 @@
-let person = {
-    Content:'Hello World, this is [Mudalue emeka] with HNGi7 ID [HNG-01029] using [javascript] for stage 2 task',
-   FullName:'Mudalue Emeka',
-    slack : 'HNG-01029',
-    language: 'javascript',
-    email:'emekachristian511@gmail.com'
-};
-let persons = JSON.stringify(person);
-console.log(persons);
