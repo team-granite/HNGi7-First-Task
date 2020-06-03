a32aa16de972a3893e113de48332ceae65e43510
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-06-03 17:59:14 +0100

    added email

--- scripts/yhkehinde.js
+++ scripts/yhkehinde.js
@@ -8,7 +8,7 @@ let cl = console.log;
 
 
 const showText = () => {
-    cl(`Hello World, this is ${person.fullName} with HNGi7 ID ${person.hngID} using ${person.progLanguage} for stage 2 task`);
+    cl(`Hello World, this is ${person.fullName} with HNGi7 ID ${person.hngID} using ${person.progLanguage} for stage 2 task  and ${person.email}`);
 };
 
 showText();
