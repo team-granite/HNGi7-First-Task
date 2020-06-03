3cde39044495163d18b44f1ad98eef4132d1dd2c
Lilian Ovrawah <lilianovrawah@gmail.com>
2020-06-03 15:08:54 +0100

    eel script

--- scripts/eel.php
+++ scripts/eel.php
@@ -1,9 +1,8 @@
 <?php 
-$firstName = 'Lilian';
-$lastName = 'Ovrawah';
+$fullname = 'Lilian Ovrawah';
 $hng_id = 'HNG-03716';
-$lang = 'PHP';
+$language = 'PHP';
 $email = 'lilianovrawah@gmail.com';
 
-echo 'Hello World, this is ' .$firstName .' ' .$lastName . ' with HNGi7 ID ' .$hng_id . ' using ' .$lang .' for stage 2 task and this is my email address ' .$email ;
+echo 'Hello World, this is ' .$fullname . ' with HNGi7 ID ' .$hng_id . ' using ' .$language .' for stage 2 task and this is my email address ' .$email ;
 ?>
