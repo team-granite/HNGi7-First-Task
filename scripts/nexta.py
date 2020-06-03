742ec98c4f76b597cdc92afd4783f894ffffbd1d
Nextafari <chinedue16@gmail.com>
2020-06-03 21:25:40 +0100

    Feat::snake:initial commit

---
+++ scripts/nexta.py
@@ -0,0 +1,10 @@
+
+def intern():
+    name = "Emmanuel Omeogah"
+    hngi7 = 2861
+    language = "Python"
+    email = "chinedue16@gmail.com"
+
+    print (f"Hello World, this is {name} with HNGi7 ID HNG-0{hngi7}, using {language} for stage two task")
+    print(f"and email {email}")
+intern()
