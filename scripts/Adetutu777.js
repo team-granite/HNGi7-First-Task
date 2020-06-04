3b0de13f026c7afcc0514b8778a2408c5bbbaf93
Adetutu Gbangbola <60041984+Adetutu777@users.noreply.github.com>
2020-06-04 23:14:53 +0100

    HNG Second Task

---
+++ scripts/Adetutu777.js
@@ -0,0 +1,12 @@
+const name= 'Adetutu Gbangbola';
+const hng= 'HNG-02928';
+const lang= 'javascript';
+const mail='adetutugbangbola@gmail.com';
+
+const profile = `Hello world, this is ${name} with HNG17 ID ${hng} using ${lang} for stage2 task with ${mail}`
+
+function showRes (){
+    console.log(profile);
+}
+
+showRes();
