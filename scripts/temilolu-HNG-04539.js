baa2ba983d5323a0c4a6c449bcf94d83aac41997
unknown <temiloluwaabiodunojo@gamail.com>
2020-06-04 19:10:48 +0100

    slack:temilolu

--- scripts/temilolu-HNG-04539.js
+++
@@ -1,7 +0,0 @@
-const ID = "HNG-04539"
-const firstName = "Temiloluwa"
-const lastName = "Abiodun"
-const language ="JavaScript"
-const email = "temiloluwaabiodunojo@gmail.com"
-const result =`Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${ID}  using ${language} for stage 2 task and ${email}`
-console.log(result)
