76c43fc80b869ed51517d7a748ca00945f08fac3
VictoriaAde <adedayovicky123@gmail.com>
2020-06-04 16:14:51 +0100

    Previous script failed, Pls merge

--- scripts/freakingSpicy.js
+++ scripts/freakingSpicy.js
@@ -2,5 +2,5 @@
     var data = '{"firstName" : "Victoria ", "lastName" : "Adedayo", "id" : "HNG-01723", "email" : "adedayovicky123@gmail.com","language" : "Javascript"}';
 
     var j = (data);
-    console.log("Hello World, My name is " + j.firstName + j.lastName + " with HNGi7 ID " + j.id + " using " + j.language + " to complete stage 2 task and email " + j.email);
+    console.log("Hello World, this is " + j.firstName + j.lastName + " with HNGi7 ID " + j.id + " using " + j.language + " to complete stage 2 task and email " + j.email);
 
