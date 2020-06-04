0caa8f86422c6222c7e78e1d3fdb947c7ca2b64b
surdman-coder <66185908+surdman-coder@users.noreply.github.com>
2020-06-04 01:34:23 +0100

    added email address
    added email address

--- scripts/Yusufsodiq.py
+++ scripts/Yusufsodiq.py
@@ -3,4 +3,4 @@ ID= 'HNG-02857'
 Lang= 'python'
 Email= 'yusufsodiq22@gmail.com'
 
-print('Hello World, this is '+Full_name+' with HNG_ID '+ID+' using the language '+Lang+' for task 2')
+print('Hello World, this is '+Full_name+' with HNG_ID '+ID+' using the language '+Lang+' for task 2 and '+Email+' is my email address')
