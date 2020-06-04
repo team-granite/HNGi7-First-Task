dc648fab9e3efebabebf74a5a4165d5027c008e4
Orevvy <66218956+Orevvy@users.noreply.github.com>
2020-06-04 11:31:27 +0100

    Update mary.js

--- scripts/mary.js
+++ scripts/mary.js
@@ -1,4 +1,4 @@
 function mary () {
-    return "Hello world, this is Enoch Mary Orevaoghene with HNGi7 ID HNG-00484 using javascript for stage 2 task with email morevaoghene@gmail.com";
+    return "Hello World, this is Enoch Mary with HNGi7 ID HNG-00484 using javascript for stage 2 task and morevaoghene@gmail.com";
 }
 console.log(mary())
