33e89678d2fa8c74e8986e19aba06085cc035208
Wealthcodest <ovobite.solomon@gmail.com>
2020-06-04 01:19:00 +0100

    Create WealthcodeX.js
    This my script - Ovobite Solomon @WealthcodeX

---
+++ scripts/WealthcodeX.js
@@ -0,0 +1,9 @@
+const name = 'Ovobite Solomon';
+const ID = 'U014SDBRL4C';
+const email = 'ovobite.solomon@gmail.com';
+const language = 'javascript';
+
+let script;
+script = 'Hello World, this is ' + name + ' with HNGi7 ID ' + ID + ' using ' + language + ' for stage 2 task';
+
+console.log(script);
