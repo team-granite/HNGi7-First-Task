178c6cb6ce8dbcc2d22a31fe8b94758d171acf1b
Okekunle Oluwatobiloba <okekunletobi@gmail.com>
2020-06-04 17:02:23 +0100

    Heroku test

--- scripts/dromoh.php
+++ scripts/dromoh.php
@@ -5,5 +5,3 @@ $my_language = "PHP";
 $email = "gabrielifoga@yahoo.com";
 
 echo "Hello World, this is ".$my_name." with HNGi7 ID ".$my_id." using ".$my_language." for stage 2 task and ". $email;
-
-?>
