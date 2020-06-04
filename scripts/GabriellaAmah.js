5ae8ea7b9cfe976f8bd44747d46e2098c8d74d7f
gabbie amah <gabbieamah49@gmail.com>
2020-06-04 06:34:27 +0100

    added my email

--- scripts/GabriellaAmah.js
+++ scripts/GabriellaAmah.js
@@ -2,9 +2,10 @@
 let   fullName =  'Gabriella Amaefule';
 let  ID =  'HNG-02535';
 let  language =  'javascript';
+let Email = 'gabbieamah49@gmail.com';
     
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2`)
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 with email ${Email}`)
     
     
 
