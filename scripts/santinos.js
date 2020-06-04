946d52b41b9c58b4b4234968a43fa4edc4d20fc5
Ephraimite <aniekanephraim@gmail.com>
2020-06-04 14:26:31 +0100

    santinos script

---
+++ scripts/santinos.js
@@ -0,0 +1,12 @@
+function myInfo() {
+    var fullname = 'Ephraim Ibatt'
+    var myID = 'HNG-03519'
+    var launguage ='JavaScript'
+    var email = 'aniekanephraim@gmail.com'
+
+    let output;
+    output = `Hello World, this is ${myInfo.fullname} with HNG ID ${myInfo.myID} using ${myInfo.launguage} and ${myInfo.email}`
+
+    
+}
+console.log(output)
