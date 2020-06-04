d1146fcfa305e01424a18a2999d095fa4df3e553
Adeola007 <adeleyeabdulbasit@yahoo.com>
2020-06-04 11:31:04 +0100

    adeolaa script

---
+++ scripts/Adeolaa.js
@@ -0,0 +1,8 @@
+const personal = {
+    name: "Adeleye Abdulbasit",
+    id: "HNG-00429",
+    language: "Javascript",
+    email: "adeleyeabdulbasit@gmail.com"
+};
+
+console.log('Hello World, this is ' + personal["name"] + ' with HNGi7 ID ' + personal["id"] + ' using ' + personal["language"] + ' for stage 2 task and ' + personal["email"])
