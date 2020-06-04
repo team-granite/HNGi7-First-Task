925e9f70e0ea759b1fa759757d18c25343df47c9
h2o4sure01 <h2o4sure01@gmail.com>
2020-06-04 07:37:15 +0100

    Olokoto Habeeb

--- scripts/IamGroot.py
+++ scripts/IamGroot.py
@@ -1,5 +1,8 @@
-Name = "Habeeb Olokoto"
-ID = 'HNG-02590'
-language = 'Python'
 
-print("Hello world, This is {}, with HNG ID {} using {} for stage 2 task ".format(Name,ID,language))
+def result():
+    Name = "Habeeb Olokoto"
+    ID = 'HNG-02590'
+    language = 'Python'
+    email = 'olokotoh@gmail.com'
+    print(f'Hello world, this is {Name} with HNG-ID {ID} using {language} for stage 2 task. {email}')
+result()
