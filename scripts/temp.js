67ad938bd01889999e439dc8037177b3f9cfa2da
dejetem <52535603+dejetem@users.noreply.github.com>
2020-06-04 15:58:50 +0100

    my new update

--- scripts/temp.js
+++ scripts/temp.js
@@ -5,7 +5,7 @@ const language = "javascript";
 const email = "enyejonathansimon@yahoo.com";
 
 const displayScript = () => {
-    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email} `);
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email}`);
 }
 
 displayScript();
