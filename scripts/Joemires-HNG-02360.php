bbc61e9b38e6944048c18d1f6ff2c3961f689d64
Joseph Bassey <Joemires20@yahoo.com>
2020-06-04 14:33:40 +0100

    Updated my script to check if ran from terminal and standard output control

--- scripts/Joemires-HNG-02360.php
+++ scripts/Joemires-HNG-02360.php
@@ -1,5 +1,5 @@
 <?php
-// $input = fgets(STDIN);
+
 $output = "Hello World, this is Joseph Bassey with HNGi7 ID HNG-02360 using PHP for stage 2 task and Joemires20@yahoo.com";
 
 if(defined('STDIN') || PHP_SAPI === 'cli')
