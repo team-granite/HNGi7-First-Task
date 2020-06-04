dc9f93a7a10f1bf34ef090584864c5da1c3c3742
udayfah <63933925+udayfah@users.noreply.github.com>
2020-06-04 12:07:53 +0100

    Create udayfah.py

---
+++ udayfah.py
@@ -0,0 +1,9 @@
+
+name = "Olaosebikan Habeebullah"
+id = "HNG-05079"
+language = "python"
+email_address = "holaosebikan005@gmail.com"
+
+info = f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task and {email_address}"
+
+print(info)
