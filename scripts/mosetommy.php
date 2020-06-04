50f410d4b863cf4976fd3f55b2216969f2f35b7f
eyitomiye olaniyi <eyitomiyeolaniyi@gmail.com>
2020-06-04 15:19:39 +0100

    re commiting my file

--- scripts/mosetommy.php
+++ scripts/mosetommy.php
@@ -1,7 +1,7 @@
 <?php
 
 $profile= [
-			'fullname'=>'Eyitomiye Olaniyi Mose',
+			'fullname'=>'Eyitomiye Olaniyi Moses',
 			'email'=>'olaniyimoses3464@gmail.com',
 			'HNGId'=>'HNG-05521',
 			'language'=>'PHP'
@@ -9,7 +9,7 @@ $profile= [
 
 function displayStatement($profile){
 
-		$statement = "Hello World, this is {$profile['fullname']} with HNGi7 ID {$profile['HNGId']} using {$profile['language']} for stage 2 task and {$profile['email']} ";
+		$statement = "Hello World, this is {$profile['fullname']} with HNGi7 ID {$profile['HNGId']} using {$profile['language']} for stage 2 task ";
 		return $statement;
 
 	}
