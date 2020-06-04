40a61190b5bc50dc07b14548a1d5913e34081432
Oluwakemi Audu <audukemi@gmail.com>
2020-06-04 07:24:50 +0100

    this is my script

---
+++ scripts/kemi.js
@@ -0,0 +1,15 @@
+//variable declaration
+function data() {
+    var name = "Oluwakemi Audu";
+    var id = "HNG-01982";
+    var email = "audukemi@gmail.com";
+    var language = "JavaScript";
+    
+    var displaydata = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email}`;
+    
+    return (displaydata);  
+} 
+
+console.log(data());
+
+
