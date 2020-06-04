0b97b239daf03d895753914baa623423f385a97d
g0thman <g.othman.anis@gmail.com>
2020-06-04 14:18:30 +0200

    g0thman fixed

--- scripts/g0thman-HNG-01952.py
+++ scripts/g0thman-HNG-01952.py
@@ -5,7 +5,7 @@
 hello = 'Hello World, this is '
 my_name = 'Othman Anis GUERHLI '
 number = 'HNG-01952 '
-language = 'python '
+language = 'Python '
 email = 'g.othman.anis@gmail.com'
 
-print(hello + my_name + 'with HNGi7 ID ' + number + 'using ' + language + 'for stage 2 task ' + email)
+print(hello + my_name + 'with HNGi7 ID ' + number + 'using ' + language + 'for stage 2 task and ' + email)
