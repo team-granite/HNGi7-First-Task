6d4bacbd970ad41cb0e58ae8d9b118277675ce9a
surdman-coder <66185908+surdman-coder@users.noreply.github.com>
2020-06-04 17:44:33 +0100

    Update Yusufsodiq.py

--- scripts/Yusufsodiq.py
+++ scripts/Yusufsodiq.py
@@ -3,4 +3,4 @@ ID= 'HNG-02857'
 Lang= 'python'
 Email= 'yusufsodiq22@gmail.com'
 
-print('Hello World, this is '+Full_name+' with HNGi7 ID '+ID+' using '+Lang+' for stage 2 task and '+Email+' is my email address')
+print('Hello World, this is '+Full_name+' with HNGi7 ID '+ID+' using '+Lang+' for stage 2 task and '+Email+'')
