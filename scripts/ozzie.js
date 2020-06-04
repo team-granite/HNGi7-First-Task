9ce7e70683777c579c437d19b8ff0b09782b597a
oziomachi <oziomaonyishi4@gmail.com>
2020-06-04 01:59:30 +0100

    added ozzie.js

--- scripts/ozzie.js
+++ scripts/ozzie.js
@@ -1,12 +1,12 @@
 var fullName = "Ugwuoke Ozioma";
-var HNGi7ID = "HNG-00610";
+var hngid = "HNG-00610";
 var language = "JavaScript";
 var email = "oziomaonyishi4@gmail.com";
 console.log(
   "Hello World, this is " +
     fullName +
     " with HNGi7 ID " +
-    HNGi7ID +
+    hngid +
     " using " +
     language +
     " for stage 2 task and " +
