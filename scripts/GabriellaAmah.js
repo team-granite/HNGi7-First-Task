1ccac93d5402e27e90babe45a7bf2d3d0d947eed
gabbie amah <gabbieamah49@gmail.com>
2020-06-04 07:02:14 +0100

    another change

--- scripts/GabriellaAmah.js
+++ scripts/GabriellaAmah.js
@@ -1,11 +1,11 @@
 
 let   fullName =  'Gabriella Amaefule';
-let  ID =  'HNG-02535';
+let  id =  'HNG-02535';
 let  language =  'javascript';
-let Email = 'gabbieamah49@gmail.com';
+let email = 'gabbieamah49@gmail.com';
     
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 with email ${Email}`)
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2`)
     
     
 
