b8dcf9f6aae9991e15551cdaa73063dbe01ff285
tegajunior <46059537+tegajunior@users.noreply.github.com>
2020-06-04 07:25:21 -0700

    add chidi.E script

---
+++ scripts/chidi.E.script.js
@@ -0,0 +1,3 @@
+//jshint esversion:6
+const output = "Hello World, this is Uzoma Chidiebere with HNGi7 ID HNG-02389 using JavaScript for stage 2 task and chidiebereuzomahumble@gmail.com";
+console.log(output);
