bb8919e4524ad4b9d0aaf1f13b81e3334f59307f
macmartins <macmartins081@gmail.com>
2020-06-04 22:03:26 +0100

    added mcmartins.py

---
+++ scripts/Mcmartins.py
@@ -0,0 +1,7 @@
+name = "Obinna Aguwa"
+hng_id = 'HNG-00129'
+language = "Python"
+email = "macmartins081@gmail.com"
+result = (f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task and {email}")
+
+print(result)
