d102e73e7b4941697fe82c0f4b387ed0587ead00
princewill Jackson <princewilljackson@ymail.com>
2020-06-04 18:18:08 +0100

    princewill jackson's updated script

--- scripts/princewilljackson-05290.py
+++ scripts/princewilljackson-05290.py
@@ -1,8 +1,6 @@
-my_output = "Hello World, this is Princewill Jackson with HNGi7 ID HNG-05290 using Python for stage 2 task and princewilljackson@ymail.com"
-
 def my_script():
-    """Displays output variable."""
-    return my_output
+    """Returns a string."""
+    return "Hello World, this is Princewill Jackson with HNGi7 ID HNG-05290 using Python for stage 2 task and princewilljackson@ymail.com"
 
 
 print(my_script())
