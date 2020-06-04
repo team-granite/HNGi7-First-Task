7a478580c2baaeb524a50e1b959a965a84cc04d6
h2o4sure01 <h2o4sure01@gmail.com>
2020-06-04 08:02:07 +0100

    added my mail address

--- scripts/IamGroot.py
+++ scripts/IamGroot.py
@@ -1,8 +1,10 @@
 
+data = {'Name': "Habeeb Olokoto",
+    'ID': 'HNG-02590',
+    'language': 'Python',
+    'email': 'olokotoh@gmail.com'}
+
 def result():
-    Name = "Habeeb Olokoto"
-    ID = 'HNG-02590'
-    language = 'Python'
-    email = 'olokotoh@gmail.com'
-    print(f'Hello world, this is {Name} with HNG-ID {ID} using {language} for stage 2 task. {email}')
+    output = ('Hello world, this is {} with HNG-ID {} using {} for stage 2 task. {}'.format(data['Name'],data['ID'],data['language'],data['email']))
+    print(output)
 result()
