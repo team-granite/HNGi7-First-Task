3727c27e92b2b9471f82d26e99e565a8c2a5ba4b
Ladeomor <64097881+Ladeomor@users.noreply.github.com>
2020-06-04 10:59:54 -0700

    Revert "My HNG task"
    This reverts commit 43e0e93d181c355ee2bd86ec6e672da056252cbc.

--- scripts/praise.py
+++
@@ -1,7 +0,0 @@
-fullname = "Praise Odeyemi"
-ID = "HNG-05451"
-language = "Python"
-email = "boluwatifeodeyemi@gmail.com"
-
-
-print('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task and %s' % (fullname, ID, language, email))
