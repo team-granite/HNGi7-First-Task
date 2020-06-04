8cbbab1b931967c15b4fb3f79e401f287eb2ba31
Falola Ibrahim A <ayofalola240@gmail.com>
2020-06-04 05:52:00 +0100

    new update

--- scripts/ibrahim.js
+++ scripts/ibrahim.js
@@ -1,8 +1,8 @@
 
 let person = {
-    fullName :"Falola Ibrahim",
-    hngID : "HNG-03833",
-    progLanguage : "javascript",
+    fullName: "Falola Ibrahim",
+    hngID: "HNG-03833",
+    progLanguage: "javascript",
     email: "ayofalola240@gmail.com"
 };
 let cl = console.log;
@@ -12,4 +12,4 @@ const printOutput = () => {
     cl(`Hello World, this is ${person.fullName} with HNGi7 ID ${person.hngID} using ${person.progLanguage} for stage 2 task  and ${person.email}`);
 };
 
-sprintOutput();
+printOutput();
