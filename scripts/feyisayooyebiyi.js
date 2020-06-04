25fade6601efbc756c6a3cfa2360e05fb46f7eb0
feyisayooyebiyi <59442702+feyisayooyebiyi@users.noreply.github.com>
2020-06-04 09:58:08 +0100

    Update feyisayooyebiyi.js
    made a fix

--- scripts/feyisayooyebiyi.js
+++ scripts/feyisayooyebiyi.js
@@ -10,7 +10,7 @@
 
 
 function myTask2(){
-    console.log('Hello World, this is Feyisayo Oyebiyi with HNGi7 IDHNG-03986 using JavaScript for stage 2 task and feyisayooyebiyi@gmail.com');
+    console.log('Hello World, this is Feyisayo Oyebiyi with HNGi7 ID HNG-03986 using JavaScript for stage 2 task and feyisayooyebiyi@gmail.com');
 };
 
 myTask2();
