500b36714f8b841094f07e41455f5e35020a0aa4
oyinfaith <61914228+oyinfaith@users.noreply.github.com>
2020-06-04 12:02:05 -0700

    Update Faithscript.js

--- scripts/Faithscript.js
+++ scripts/Faithscript.js
@@ -4,5 +4,5 @@ var programmingLanguage ='JavaScript';
 var emailAddress = 'lacrimy06@gmail.com';
 
 
-console.log('Hello world, ', 'this is '+ Name +' with '+  hngId +' using '
+console.log('Hello World, ', 'this is '+ Name +' with '+  hngId +' using '
 +  programmingLanguage + ' for stage 2 '+'with '+'email ' + emailAddress);
