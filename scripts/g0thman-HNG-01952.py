de8be86b4410f9676f375aa2932bc445a5fbea31
root <root@DESKTOP-NPT3HRC.localdomain>
2020-06-04 14:47:41 +0200

    2 firstnames bug

--- scripts/g0thman-HNG-01952.py
+++ scripts/g0thman-HNG-01952.py
@@ -3,7 +3,7 @@
 #"Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task".
 
 hello = 'Hello World, this is '
-my_name = 'Othman Anis GUERHLI '
+my_name = 'Othman GUERHLI '
 number = 'HNG-01952 '
 language = 'Python '
 email = 'g.othman.anis@gmail.com'
