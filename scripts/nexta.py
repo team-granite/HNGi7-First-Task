08a35896346ca7d4013643b1f69cb46f58575adc
Nextafari <chinedue16@gmail.com>
2020-06-04 11:27:18 +0100

    Fix::snake:fixed code

--- scripts/nexta.py
+++ scripts/nexta.py
@@ -1,9 +1,9 @@
 
 def intern():
     name = "Emmanuel Omeogah"
-    hngi7 = 2861
+    hngi7 = "HNG-02861"
     language = "Python"
     email = "chinedue16@gmail.com"
 
-    print (f"Hello World, this is {name} with HNGi7 ID HNG-0{hngi7}, using {language} for stage two task and email {email}")
+    print (f"Hello World, this is {name} with HNGi7 ID {hngi7}, using {language} for stage two task and email {email}")
 intern()
