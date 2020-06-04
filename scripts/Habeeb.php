a4e9b756747f0049657a448d7752bb7a1b0a311a
AdeCode <34035122+AdeCode@users.noreply.github.com>
2020-06-04 16:11:03 +0100

    Adding my PHP script file

--- scripts/Habeeb.php
+++ scripts/Habeeb.php
@@ -3,5 +3,5 @@
     $hngId= "HNG-04098";
     $language = "PHP";
     $email = "agorohabeeb@gmail.com";
-    echo "Hello World, this is ".$name." with HNGi7 ID".$hngId." using ".$language." for stage 2 task and ".$email;
+    echo "Hello World, this is ".$name." with HNGi7 ID ".$hngId." using ".$language." for stage 2 task and ".$email;
 ?>
