f50eb9b264a66422729dd305a9a9c09c511b6252
Ledisi Zabbey <ledisizab@gmail.com>
2020-06-04 15:28:00 +0100

    Update to file

--- Ledy.js
+++
@@ -1,6 +0,0 @@
-let name = "Ledisi Zabbey";
-let id = "HNGi7-01980";
-let lang = "JavaScript"
-let email = "ledisizab@gmail.com";
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for task 2 and ${email}`);
---
+++ scripts/Ledy.js
@@ -0,0 +1,5 @@
+function ledisi() {
+    console.log("Hello World, this is Ledisi Zabbey with HNGi7 ID HNG-01980 using Javascript for stage 2 task and ledisizab@gmail.com");
+}
+
+ledisi();
