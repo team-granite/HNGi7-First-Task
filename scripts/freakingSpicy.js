d897e30b6ec4fdef134026ea14c1252d481f8359
VictoriaAde <adedayovicky123@gmail.com>
2020-06-04 18:02:04 +0100

    Merge

--- scripts/freakingSpicy.js
+++ scripts/freakingSpicy.js
@@ -1,6 +1,5 @@
 
-    var data = '{"firstName" : "Victoria ", "lastName" : "Adedayo", "id" : "HNG-01723", "email" : "adedayovicky123@gmail.com","language" : "Javascript"}';
+var data = '{"first_name":"Victoria", "last_name":"Adedayo", "id":"HNG-01723", "email":"adedayovicky123@gmail.com", "language":"Javascript"}';
 
-    var j = (data);
-    console.log("Hello World, this is " + j.firstName + j.lastName + " with HNGi7 ID " + j.id + " using " + j.language + " to complete stage 2 task and email " + j.email);
-
+var j = JSON.parse(data);
+console.log("Hello World, this is " + j.first_name + " " + j.last_name + " with HNGi7 ID " + j.id + " using " + j.language + " for stage 2 task and " + j.email);
