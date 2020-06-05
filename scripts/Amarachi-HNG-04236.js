21dc404c9ccc3e5f6143aa7aba8ebbd21f4a5e0f
Amarachi9 <59516730+Amarachi9@users.noreply.github.com>
2020-06-05 20:14:19 +0100

    Update Amarachi-HNG-04236.js

--- scripts/Amarachi-HNG-04236.js
+++ scripts/Amarachi-HNG-04236.js
@@ -1,7 +1,13 @@
-var fullName = 'Amarachi Ogbonnaya';
-var ID = 'HNG-04236';
-var language = 'Javascript';
-var email = "amarachiogbo19@gmail.com";
+let fullName = 'Amarachi Ogbonnaya';
+let ID = 'HNG-04236';
+let language = 'Javascript';
+let email = "amarachiogbo19@gmail.com";
 
-console.log('Hello World, this is ' + fullName, + ' HNGi7 ID ' + ID + ' using ' +
-language + ' for stage 2 task ' + ' and ' + email);
+console.log('Hello World, this is ' + 
+fullName, + 
+' HNGi7 ID ' +
+ ID + 
+' using ' +
+language + 
+' for stage 2 task ' + ' and ' + 
+email);
