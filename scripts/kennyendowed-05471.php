60b7957960dce44eba988751ea99973561db51bd
kenneth Endowed <kenneyg50@gmail.com>
2020-06-05 19:53:16 +0100

    Add files via upload

---
+++ scripts/kennyendowed-05471.php
@@ -0,0 +1,13 @@
+<?php
+$name = "kenneth Akpan";
+$hng_id = "HNG-05471";
+$language = "PHP";
+$email = "kenneyg50@gmail.com";
+
+function show_info(){
+    global $intern, $hng_id, $language, $email;
+    return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
+}
+
+echo show_info();
+?>
