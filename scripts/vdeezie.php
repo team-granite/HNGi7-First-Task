80e09d11d98214a28b18936918d2840575b1b002
Victor Desire <61976018+vdeezie@users.noreply.github.com>
2020-06-05 23:58:54 +0100

    commit

--- scripts/vdeezie.php
+++ scripts/vdeezie.php
@@ -1,8 +1,13 @@
-<?php 
-$fullname = 'Victor Desire';
-$hng_id = 'HNG-01598';
-$language = 'PHP';
-$email = 'lilianovrawah@gmail.com';
+<?php
+$fullName = "Victor Desire";
+$HNG_Id = "HNG-01598";
+$language = "PHP";
+$email = "edesire22@yahoo.com";
 
-echo 'Hello World, this is ' .$fullname . ' with HNGi7 ID ' .$hng_id . ' using ' .$language .' for stage 2 task and this is my email address ' .$email ;
+echo returnContent($fullName, $HNG_Id, $language, $email);
+
+function returnContent($fullName, $HNG_Id, $language, $email) {
+$content = "Hello World, this is ".$fullName." with HNGi7 ID ".$HNG_Id." using ".$language." for stage 2 task and this is my email address ".$email;
+return $content;
+} 
 ?>
