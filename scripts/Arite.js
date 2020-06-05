6a1480a6696bf9985555465db5fba8a2244aba03
Arhyte <ariteomuabor@gmail.com>
2020-06-05 23:54:04 +0100

    Arites's stage-1

---
+++ scripts/Arite.js
@@ -0,0 +1,10 @@
+var internParameters = {
+    firstName: 'Arite',
+    lastName: 'Omuabor',
+    internID: 'HNG-00192',
+    language: 'JavaScript'
+};
+
+var outputMessage = "Hello World,"+ " this is " + internParameters.firstName+ " " + internParameters.lastName + " with HNGi7 ID " + internParameters.internID + " using "+ internParameters.language + " for stage 2 task.";
+
+console.log(outputMessage);
