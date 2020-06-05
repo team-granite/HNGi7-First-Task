738368829027a3a70a56ac3296e34394d56218ec
adujoseph <iamjosephadu@gmail.com>
2020-06-05 15:33:06 +0100

    update

--- scripts/joseph.js
+++ scripts/joseph.js
@@ -1,5 +1,5 @@
 const email = 'adujosephayo@gmail.com';
 function task(name, id, language) {
-    return (`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`)
+    return (`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task and email is adujosephayo@gmail.com`)
 }
 console.log(task('Joseph Adu', "HNG-05313", 'javascript'));
