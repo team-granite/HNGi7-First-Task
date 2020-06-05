6ee92720ed57167ad963a356542e975f0daa22c0
nancyben <66383239+nancyben@users.noreply.github.com>
2020-06-05 01:48:37 +0100

    Update medulabbb.js

--- scripts/medulabbb.js
+++ scripts/medulabbb.js
@@ -1,8 +1,8 @@
-const firstName = "Nancy"
-const lastName = "Ben"
-const hngID = "HNG-06537"
-const language = "javascript"
-const email = "medulab10@gmail.com"
+let firstName = "Nancy"
+let lastName = "Ben"
+let hngID = "HNG-06537"
+let language = "javascript"
+let email = "medulab10@gmail.com"
 
 console.log(
     `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task and my email is ${email}`
