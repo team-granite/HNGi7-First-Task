7b4334e62ab1a1fcf97f154d613533050720bc3b
Marvellous Ubani <marvellousubani@gmail.com>
2020-06-05 14:26:24 +0100

    added searchbar feature

--- index.php
+++ index.php
@@ -125,6 +125,19 @@ if (isset($_GET['json'])) {
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <!-- Bootstrap CSS -->
         <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+        <style> .hidden{ display:none;} #searchbar{padding: .2rem 1rem;}</style>
+        <script>
+            const filterChange = result =>{
+                let people = document.querySelectorAll("tr");
+                people.forEach( person => {
+                    if(person.children[0].textContent.includes(result.value) || person.children[0].textContent.includes("Full Name")){
+                        person.classList.remove("hidden");
+                    }else{
+                        person.classList.add("hidden");
+                    }
+                })
+            }
+        </script>
     </head>
 
     <body>
@@ -137,6 +150,9 @@ if (isset($_GET['json'])) {
                 <div class='col-1'></div>
                 <div class='col-2'>
                     <span class='btn btn-success btn-disabled btn-sm btn-block'><?php echo $submitted ?> Submitted</span>
+                </div>
+                <div class='col-6'>
+                  <input type="text" placeholder="Search for Your Name" id="searchbar" onkeyup="filterChange(this)"/>
                 </div>
             </div>
         </div>
@@ -290,6 +306,7 @@ if (isset($_GET['json'])) {
     <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
+    
 
     </html>
 <?php } ?>
