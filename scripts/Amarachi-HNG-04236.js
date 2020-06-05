23eed71a4b120ebbe20a29708f517e15375f8e79
Amarachi <amarachiogbo19@gmail.com>
2020-06-05 04:11:36 +0100

    testing testing

---
+++ scripts/Amarachi-HNG-04236.js
@@ -0,0 +1,7 @@
+var fullName = 'Amarachi Ogbonnaya';
+var ID = 'HNG-04236';
+var language = 'Javascript';
+var email = "amarachiogbo19@gmail.com";
+
+console.log('Hello World, this is ' + fullName + ' HNGi7 ID ' + ID + ' using ' +
+language + ' and ' + email + ' for stage 2 task ');
