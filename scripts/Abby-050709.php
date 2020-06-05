d6424052c576d125ad8edc5ce3ab028a7d59a7e3
Majesty-stack <60063709+Majesty-stack@users.noreply.github.com>
2020-06-05 14:26:35 +0100

    Update Abby-050709.php

--- scripts/Abby-050709.php
+++ scripts/Abby-050709.php
@@ -1,14 +1,14 @@
 <?php
-$intern_info= [
-			'fullname'=>'Amusa Quadri Abiodun',
-			'email'=>'amusaabiodun88@gmail.com',
-			'HNGId'=>'HNG-050709',
-			'language'=>'PHP'
-			];
-                    function displayStatement($intern_info){
-		$statement = "Hello World, this is {$intern_info['fullname']} with HNGi7 ID {$intern_info['HNGId']} using {$intern_info['language']} for stage 2 task ";
-		return $statement;
-	}
-	echo displayStatement($intern_info);
+$name = "Amusa Quadri";
+$hng_id = "HNG-050709";
+$language = "PHP";
+$email = "amusaabiodun88@gmail.com";
+
+function show_info(){ 
+    global $intern, $hng_id, $language, $email;
+    return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
+}
+
+echo show_info();
 ?>
 
