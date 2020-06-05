f20225628dc86bc984b75c3c52f0c3f3bc191385
Save <46031366+Doopra@users.noreply.github.com>
2020-06-05 19:20:34 +0100

    Create save.php

---
+++ scripts/save.php
@@ -0,0 +1,7 @@
+<?php
+ $name = "Saviour Mkpong";
+ $language = "PHP";
+ $hngid = "HNG-04531";
+ $mail = "mkpongsaviour7@gmail.com";
+ echo "Hello World, this is $name with HNGi7 ID $hngid using $language for stage 2 task and email $mail";
+?>
