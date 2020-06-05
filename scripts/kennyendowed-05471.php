b811655f67772cdf43f868eb62213997c0ee276b
kenneth Endowed <kenneyg50@gmail.com>
2020-06-05 20:00:09 +0100

    Update kennyendowed-05471.php

--- scripts/kennyendowed-05471.php
+++ scripts/kennyendowed-05471.php
@@ -5,7 +5,7 @@ $language = "PHP";
 $email = "kenneyg50@gmail.com";
 
 function show_info(){
-    global $intern, $hng_id, $language, $email;
+    global  $hng_id, $language, $email;
     return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
 }
 
