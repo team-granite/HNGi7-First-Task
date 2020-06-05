19746db5aab92fb418933f33239ee7b0b7283cf0
Olaegbe Samuel <25031528+goodhands@users.noreply.github.com>
2020-06-05 01:04:06 +0100

    Corrected HNG id

--- scripts/goodhands.php
+++ scripts/goodhands.php
@@ -1,3 +1,3 @@
 <?php
 
-    echo "Hello World, this is Samuel Olaegbe with HNGi7 ID HNG-05071 using PHP for stage 2 task and olaegbesamuel@gmail.com";
+    echo "Hello World, this is Samuel Olaegbe with HNGi7 ID HNG-05170 using PHP for stage 2 task and olaegbesamuel@gmail.com";
