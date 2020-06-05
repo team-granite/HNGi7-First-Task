6d6491c8671f991a89665fa0d0024742365fe4aa
Donmohaa1 <alaricdemahom11@gmail.com>
2020-06-05 16:58:23 +0300

    chatt.py

--- scripts/chatt.py
+++ scripts/chatt.py
@@ -5,5 +5,5 @@ person = '{"student":[{"fullname": "Mohamed Ali", "hngi7": "HNG-05487", "languag
 
 person2 = json.loads(person)
 person3 = person2["student"]
-print("Hello world, this is " + person3[0]["fullname"] + " with HNGi7 ID " + person3[0]["hngi7"] + " using " + person3[0][
+print("Hello World, this is " + person3[0]["fullname"] + " with HNGi7 ID " + person3[0]["hngi7"] + " using " + person3[0][
     "language"] + " for stage 2 task and " + person3[0]["email"])
