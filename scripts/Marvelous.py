0e97fc04e5822b3f7add7b9e22a9fa4fe2eaee58
marvelous-ikechi <marvelike7@gmail.com>
2020-06-05 19:09:15 +0100

    Marvelous Ikechi script

--- scripts/Marvelous.py
+++ scripts/Marvelous.py
@@ -1,6 +1,6 @@
 name = "Marvelous Ikechi"
-id = "INTERNSHIP ID: HNG-06329"
+id = "HNG-06329"
 email = "marvelike7@gmail.com"
 lang = "Python"
 
-print("Hello world, this is {}, with {}, using {} for stage 2 task, and email address {}".format(name, id, lang, email))
+print("Hello world, this is {}, with HNGi7 ID {} using {} for stage 2 task and  {}".format(name, id, lang, email))
