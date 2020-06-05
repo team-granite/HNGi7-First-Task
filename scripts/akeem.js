90cf36d5bfa7181c1c9cc85deb3bf404a6e2091a
agbajeolawale <agbajeolawale@outlook.com>
2020-06-05 14:14:45 +0100

    akeem's commit

---
+++ scripts/akeem.js
@@ -0,0 +1,17 @@
+var inputs = ['Akeem Agbaje', 'HNG-03992', 'Javascript', 'agbajeolawale@outlook.com'];
+
+
+let full_name = inputs[0];
+let ID = inputs[1];
+let lang_used = inputs[2];
+let email = inputs[3];
+
+var outputs = function() {
+    
+    console.log("Hello World, this is " + full_name + " with HNGi7 ID " + ID + " using "  + lang_used + " for stage 2 task and " + email);
+    
+    
+    
+}
+
+outputs();
