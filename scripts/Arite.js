e448883c7ece75161693f46cbbd3dc6db94c8e97
Arhyte <ariteomuabor@gmail.com>
2020-06-06 00:38:03 +0100

    added and email

--- scripts/Arite.js
+++ scripts/Arite.js
@@ -2,9 +2,10 @@ var internParameters = {
     firstName: 'Arite',
     lastName: 'Omuabor',
     internID: 'HNG-00192',
-    language: 'JavaScript'
+    language: 'JavaScript',
+    email: 'ariteomuabor@gmail.com'
 };
 
-var outputMessage = "Hello World,"+ " this is " + internParameters.firstName+ " " + internParameters.lastName + " with HNGi7 ID " + internParameters.internID + " using "+ internParameters.language + " for stage 2 task.";
+var outputMessage = "Hello World,"+ " this is " + internParameters.firstName+ " " + internParameters.lastName + " with HNGi7 ID " + internParameters.internID + " using "+ internParameters.language + " for stage 2 task and " + internParameters.email;
 
 console.log(outputMessage);
