93657c7030de4919371021a9f1b9316d36303187
Vchuks <64593598+Vchuks@users.noreply.github.com>
2020-06-05 18:19:13 +0100

    HNG-04054

---
+++ scripts/onyii.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+	
+
+<p id="task"> </p>
+
+<script type="text/javascript">
+
+var name="CHUKWUMEE ONYINYE";
+var num="HNG-04054";
+var lang="JAVASCRIPT";
+var email="vchuks4all@yahoo.com";
+
+document.getElementById("task").innerHTML="HELLO WORLD, THIS IS "
++name+" WITH HNGi7 ID "+num+" USING "+lang+" FOR STAGE 2 TASK AND "+email;
+</script>
+
+	</body>
+</html>
