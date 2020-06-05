269ff6a7044309fa941f9887dc2e542b57d74b9b
nancyben <66383239+nancyben@users.noreply.github.com>
2020-06-05 01:27:14 +0100

    Create medulabbb.js

---
+++ scripts/medulabbb.js
@@ -0,0 +1,9 @@
+const firstName = "Nancy"
+const lastName = "Ben"
+const hngID = "HNG-06537"
+const language = "javascript"
+const email = "medulab10@gmail.com"
+
+console.log(
+    `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task and my email is ${email}`
+)
