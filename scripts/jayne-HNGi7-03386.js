368829abfe5fabedfa89d829be57343310d0a0d6
Suggia <61287711+Suggia@users.noreply.github.com>
2020-06-05 15:26:57 +0100

    Update jayne-HNGi7-03386.js

--- scripts/jayne-HNGi7-03386.js
+++ scripts/jayne-HNGi7-03386.js
@@ -3,6 +3,6 @@ let HNGi7ID = "HNG-03386";
 let lang = "JavaScript";
 let email = "jayneessien@gmail.com";
 
-let output= `Hello World, this is ${name} with HNGi7 ID ${HNGi7ID} using ${lang} for my stage 2 task and ${email}.`
+let output= `Hello World, this is ${name} with HNGi7 ID ${HNGi7ID} using ${lang} for stage 2 task and ${email}.`
 
 console.log(output)
