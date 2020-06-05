71e9bad8a2b4f21a690be75b8c4b53b5391d534c
Emmanuel <eudoh940@gmail.com>
2020-06-05 23:26:26 +0100

    task

--- scripts/Izu.js
+++ scripts/Izu.js
@@ -1,6 +1,6 @@
-function add(fullname, id, language, email) {
-    return (`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email}`)
+function intr(name, id, lang, email) {
+    return (`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task and ${email}`)
     
 }
-var newAdd = add("Izuchukwu Iwuchuckwu", "HNG-01559", "Javascript", "izuchuksiwuh@yahoo.com");
-console.log(newAdd)
+var newIntr = intr("Izuchukwu Iwuchuckwu", "HNG-01559", "Javascript", "izuchuksiwuh@yahoo.com");
+console.log(newIntr)
