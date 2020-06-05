a7409110bf9865c3056006e00f679eeb6c2d8cd3
mcjulie <oluwadarasiwa@gmail.com>
2020-06-05 13:30:14 +0100

    added marc.js

---
+++ marc.js
@@ -0,0 +1,3 @@
+var m ='{"first_name":"Temitayo", "last_name":"Jegede", "id":"HNG-00200", "email":"oluwadarasiwa@gmail.com", "language":"Javascripts"}';
+        var obj = JSON.parse(m);
+        console.log=("Hello World, this is "+obj.first_name+" "+obj.last_name +" with HNGi7 ID "+obj.id +" using "+obj.language+"for stage 2 task Email "+obj.email);
