9c3bde41ad5a71565369ef81804c7c1e5601f7d5
DavidJoanes <DavidJoanes@users.noreply.github.com>
2020-06-05 01:16:06 +0100

    Update Joanes.js

--- scripts/Joanes.js
+++ scripts/Joanes.js
@@ -1,16 +1,1 @@
-
-let fullName = "David Kemdirim"
-let HNG_ID = "HNG-00819"
-let email = "david.kemdirim@yahoo.com"
-let language = "javascript"
-
-let details = {
-    fullName,
-    HNG_ID,
-    email,
-    language
-}
-
-console.log(`Hello World, this is David Kemdirim  with HNGi7 ID: HNG-00819 and david.kemdirim@yahoo.com using JavaScript  for stage 2 task`)
-
-
+console.log("Hello World, this is David Kemdirim  with HNGi7 ID: HNG-00819 and david.kemdirim@yahoo.com using JavaScript  for stage 2 task")
