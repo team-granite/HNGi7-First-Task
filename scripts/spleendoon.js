68c6890552c01ff34347fdebd93ca59b34030c66
splendon12 <66358582+splendon12@users.noreply.github.com>
2020-06-05 01:12:46 +0100

    First script

---
+++ scripts/spleendoon.js
@@ -0,0 +1,9 @@
+const firstName = "Onwuka"
+const lastName = "Kelvin"
+const hngID = "HNG-06278"
+const language = "javascript"
+ const email = "hax0r3dg3@gmail.com"
+
+console.log(
+    `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task and my email is ${email}`
+)
