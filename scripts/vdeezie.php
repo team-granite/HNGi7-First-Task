aa9b8fb6a446b64e2f0c057336a5c3df0249a05a
Victor Desire <61976018+vdeezie@users.noreply.github.com>
2020-06-05 23:49:34 +0100

    email

--- scripts/vdeezie.php
+++ scripts/vdeezie.php
@@ -4,10 +4,10 @@ $HNG_Id = "HNG-01598";
 $language = "PHP";
 $email = "edesire22@yahoo.com";
 
-echo returnContent($fullName, $HNG_Id, $language);
+echo returnContent($fullName, $HNG_Id, $language, $email);
 
-function returnContent($fullName, $HNG_Id, $language) {
-$content = "Hello World, this is ".$fullName." with HNGi7 ID ".$HNG_Id." using ".$language." for stage 2 task and this is my email address ".$email." ;
+function returnContent($fullName, $HNG_Id, $language, $email) {
+$content = "Hello World, this is ".$fullName." with HNGi7 ID ".$HNG_Id." using ".$language." for stage 2 task and this is my email address ".$email;
 return $content;
 } 
 ?>
