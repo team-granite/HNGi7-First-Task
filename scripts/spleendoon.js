8698beb38396939354a640de471a415d22ee63e3
splendon12 <66358582+splendon12@users.noreply.github.com>
2020-06-05 01:50:31 +0100

    Update spleendoon.js

--- scripts/spleendoon.js
+++ scripts/spleendoon.js
@@ -1,8 +1,8 @@
-const firstName = "Onwuka"
-const lastName = "Kelvin"
-const hngID = "HNG-06274"
-const language = "javascript"
- const email = "hax0r3dg3@gmail.com"
+let firstName = "Onwuka";
+let lastName = "Kelvin";
+let hngID = "HNG-06274";
+let language = "javascript";
+let email = "hax0r3dg3@gmail.com";
 
 console.log(
     `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngID} using ${language} for stage 2 task and my email is ${email}`
