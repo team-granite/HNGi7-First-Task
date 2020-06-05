b9a812129b39c0f0063c78249d3aed3f92d38399
mcjulie <oluwadarasiwa@gmail.com>
2020-06-05 16:03:02 +0100

    added marc.js

--- marc.js
+++ marc.js
@@ -1,3 +1,20 @@
-var m ='{"first_name":"Temitayo", "last_name":"Jegede", "id":"HNG-00200", "email":"oluwadarasiwa@gmail.com", "language":"Javascripts"}';
-        var obj = JSON.parse(m);
-        console.log=("Hello World, this is "+obj.first_name+" "+obj.last_name +" with HNGi7 ID "+obj.id +" using "+obj.language+"for stage 2 task Email "+obj.email);
+// //variable declaration
+// var fullName = "Jegede Temitayo";
+// var ID = "HNG-00200";
+// var language = "javascript"
+
+
+//defining the output
+let output = "Hello World, this is Jegede Temitayo with HNGi7 ID HNG-00200 using javascript for stage 2 task and oluwadarasiwa@gmail.com";
+
+
+//defining the function to output 
+function outputScript() {
+
+    //logging the output
+    console.log(output);
+}
+
+//calling the function created
+outputScript();
+
