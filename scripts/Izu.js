c25411739a989723ccb09843f336fa200a6baa60
Emmanuel <eudoh940@gmail.com>
2020-06-05 23:12:27 +0100

    my task

---
+++ scripts/Izu.js
@@ -0,0 +1,6 @@
+function add(fullname, id, language, email) {
+    return (`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email}`)
+    
+}
+var newAdd = add("Izuchukwu Iwuchuckwu", "HNG-01559", "Javascript", "izuchuksiwuh@yahoo.com");
+console.log(newAdd)
