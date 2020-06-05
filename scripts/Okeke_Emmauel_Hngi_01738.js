12e1741f470b8463fd621ed113e7bd24d50a7898
Michael Olusoji <molusoj@gmail.com>
2020-06-05 17:08:28 +0100

    Revert "Okeke Emmanuel"

--- scripts/Okeke_Emmauel_Hngi_01738.js
+++
@@ -1,7 +0,0 @@
-/*variable declaration*/
-/*My first Hngi7 task under team granite*/
-Var internName="Okeke Emmanuel"
-Var internId="HNG-01738"
-Var internLanguage="JavaScript"
-Var internEmail="ceomaobi@gmail.com"
-Console.log("Hello world this is ${internName} with HNGi7 ID ${internId} using ${intern Language} for stage two task ${internEmail}")
