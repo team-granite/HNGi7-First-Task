a6f7ad0f6e12aca8b75edbce06407b4d420d2eb0
Dani-code-opsC <danielamoo35@gmail.com>
2020-06-05 19:49:28 +0100

    added my script file written with python

---
+++ scripts/amoodaniel_.py
@@ -0,0 +1,8 @@
+def task2():
+    '''AMOO DANIEL from Team GRANITE TASK 2 '''
+    id = 'HNG-02816'
+    name = 'Amoo Daniel'
+    lang= 'Python'
+    mail = 'danielamoo35@gmail.com'
+    print("Hello World, this is {0} with HNGi7 ID {1} Using {2} for stage 2 task and {3}". format(name, id, lang, mail))
+task2()
