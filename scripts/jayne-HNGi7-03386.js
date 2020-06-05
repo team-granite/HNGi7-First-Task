3fb18dda7106eca30d4782c6279344145efb8872
Suggia <61287711+Suggia@users.noreply.github.com>
2020-06-05 10:54:24 +0100

    Delete jayne-HNGi7-03386.js

--- scripts/jayne-HNGi7-03386.js
+++
@@ -1,9 +0,0 @@
-let name= "Jane Essien";
-update
-let HNGi7ID = "HNG-03386";
-let lang = "JavaScript";
-let email = "jayneessien@gmail.com";
-
-let output= `Hello World, this is ${name} with HNGi7 ID ${HNGi7ID} using ${lang} for my stage 2 task and ${email}.`
-
-console.log(output)
