0460489e05fd0a15fe8ff60a30ad144fecc18e2f
DavidJoanes <63173423+DavidJoanes@users.noreply.github.com>
2020-06-05 14:29:18 +0100

    Add files via upload

--- scripts/Joanes.js
+++ scripts/Joanes.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is David Kemdirim  with HNGi7 ID: HNG-00819 and david.kemdirim@yahoo.com using JavaScript  for stage 2 task")
+console.log("Hello World, this is David Kemdirim with HNGi7 ID: HNG-00819 and david.kemdirim@yahoo.com using Javascript for stage 2 task")
