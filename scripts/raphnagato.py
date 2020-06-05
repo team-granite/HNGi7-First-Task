d1bd244dd66ef0654636782a9f128b934589f84e
Raphael Okeibunor <raphaelnagato19@gmail.com>
2020-06-05 23:41:50 +0100

    edit my script

--- scripts/raphnagato.py
+++ scripts/raphnagato.py
@@ -4,8 +4,8 @@ language = 'python'
 id_number = '05154'
 
 def print_data():
-    data = f'Hello World, this is {name} with HNGi7 ID HNG-{id_number}' + \
-        f' using Python for stage 2 task and {email}'
+    data = 'Hello World, this is {} with HNGi7 ID HNG-{}'.format(name,id_number) + \
+        ' using Python for stage 2 task and {}'.format(email)
 
     print(data)
 print_data()
