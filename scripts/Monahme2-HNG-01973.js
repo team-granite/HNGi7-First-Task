c285422349a8baa5cc159c6656e4e945f25c78a4
monahme <mainaremmy294@gmail.com>
2020-06-05 19:18:41 +0300

    Monahme2-HNG-01973 script

---
+++ scripts/Monahme2-HNG-01973.js
@@ -0,0 +1,6 @@
+let fullname="Remonah Makwata";
+let ID="HNG-01973";
+let emailaddress="mainaremmy294@gmail.com";
+let language="JavaScript";
+
+console.log("Hello World, this is "+fullname+" with HNGi7 ID "+ID+" using "+language+" for stage 2 task and email "+emailaddress);
