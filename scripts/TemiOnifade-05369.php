f86e99137d3cade4cbcc0188534ac78e6d352967
Temi Onifade <temmieonifade@gmail.com>
2020-06-05 20:28:25 +0100

    TemiOnifade-HNG-05369 script

---
+++ scripts/TemiOnifade-05369.php
@@ -0,0 +1,8 @@
+<?php 
+$full_name = 'Temilola Onifade';
+$hng_id = 'HNG-05369';
+$language = 'PHP';
+$email = 'temmieonifade@gmail.com';
+
+echo 'Hello World, this is ' .$full_name . ' with HNGi7 ID ' .$hng_id . ' using ' .$language .' for stage 2 task and this is my email address ' .$email ;
+?>
