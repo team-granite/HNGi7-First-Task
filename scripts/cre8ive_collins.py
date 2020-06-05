d8fba546f78130ba3a0bdb26ef39ead14f3b7f6e
Collins Wilson <wilsoncollins44@yahoo.com>
2020-06-05 17:14:54 +0100

    Re re updated

--- scripts/cre8ive_collins.py
+++ scripts/cre8ive_collins.py
@@ -1,8 +1,6 @@
 
 
 
-info = "Hello World, this is Collins Wilson with HNGi7 ID HNG-04747 using Python for stage 2 task"
-email = "wilsoncollins44@yahoo.com"
+info = "Hello World, this is Collins Wilson with HNGi7 ID HNG-04747 using Python for stage 2 task and wilsoncollins44@yahoo.com"
 
-
-print("{} and {}".format({info},{email}))
+print(info)
