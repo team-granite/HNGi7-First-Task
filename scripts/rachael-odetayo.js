9d1172138a5ea7311f8d75ed442cbc8e779ebdd2
Rakielle <bukolaodetayo@gmail.com>
2020-06-05 07:27:57 +0100

    updated my details

--- scripts/rachael-odetayo.js
+++ scripts/rachael-odetayo.js
@@ -2,9 +2,10 @@
 var fullName = "Rachael Odetayo";
 var ID = "HNG-03627";
 var language = "javascript"
+var Email = "bukolaodetayo@gmail.com"
 
 
-let output = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task and ${Email}`;
 
 
 
