bddf359866623a97da5e641132d308c344889cce
Rakielle <bukolaodetayo@gmail.com>
2020-06-05 07:14:19 +0100

    adds my details to the output

---
+++ scripts/rachael-odetayo.js
@@ -0,0 +1,19 @@
+
+var fullName = "Rachael Odetayo";
+var ID = "HNG-03627";
+var language = "javascript"
+
+
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+
+
+
+function outputScript() {
+
+
+    console.log(output);
+}
+
+
+outputScript();
+
