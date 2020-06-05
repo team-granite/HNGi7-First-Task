c2fd4b6c361e8c43af165024bc36d5291bc71aa8
Fish Underlings <fishunderlings@gmail.com>
2020-06-05 06:24:21 +0100

    made corrections to the output format

--- scripts/newton.py
+++ scripts/newton.py
@@ -3,4 +3,4 @@ email = 'ayomipoe@gmail.com'
 hngi7_id = 'HNG-00032'
 language = 'python'
 
-print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task and {}'.format(name, email, hngi7_id, language))
+print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task and {}".format(name, hngi7_id, language, email))
