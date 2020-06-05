5d92a4928234d1de8223c913b366d99d83a7e0a1
oluwamaureen <66250841+oluwamaureen@users.noreply.github.com>
2020-06-05 16:48:31 +0100

    Delete maureen_script.py

--- maureen_script.py
+++
@@ -1,6 +0,0 @@
-#print ("Hello world, this is Maureen Onabajo with HNGi7 ID 01726 using Python for stage 2 task")
-full_name = "Maureen Onabajo"
-HNGi_ID = "01726"
-language_used = "Python"
-msg = f'Hello world, this is {full_name} with HNGi7 ID {HNGi_ID} using {language_used} for stage 2 task'
-print(msg)
