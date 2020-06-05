30390222b962561b0d2b0430a5727ba627860bf3
Amarachi9 <59516730+Amarachi9@users.noreply.github.com>
2020-06-05 19:58:01 +0100

    Update Amarachi-HNG-04236.js

--- scripts/Amarachi-HNG-04236.js
+++ scripts/Amarachi-HNG-04236.js
@@ -3,5 +3,5 @@ var ID = 'HNG-04236';
 var language = 'Javascript';
 var email = "amarachiogbo19@gmail.com";
 
-console.log('Hello World, this is ' + fullName + ' HNGi7 ID ' + ID + ' using ' +
-language + ' and ' + email + ' for stage 2 task ');
+console.log('Hello World, this is ' + fullName, + ' HNGi7 ID ' + ID + ' using ' +
+language + ' for stage 2 task ' + ' and ' + email);
