a030d4156fd990b76cf1ad22742e9deb56b049c1
Utibe Nkereuwem <utibendarake5@gmail.com>
2020-06-06 15:10:14 +0100

    my first commit

---
+++ scripts/UtibeNkereuwem.py
@@ -0,0 +1,7 @@
+import json
+
+def show_details():
+    print("Hello World, this is {name} with HNGi7 ID {ID} using {language} for stage 2 task and {email}".format(name = 'Utibe Nkereuwem', ID = 'HNG-05361', language = 'Python', email = 'utibendarake5@gmail.com'))
+
+
+show_details()
