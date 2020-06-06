7f487237489b3cb49b306edaeede430d5e7365cc
Tseyi Agharaye <agharayetseyi@gmail.com>
2020-06-06 20:28:25 +0100

    added my working script

---
+++ scripts/ashagzy.js
@@ -0,0 +1,8 @@
+
+   let output = "Hello World, this is Agharaye Tseyi with HNGi7 ID HNG-06261 using javascript for stage 2 task and agharayetseyi@gmail.com"
+
+   function display (){
+       console.log(output);
+   }
+   
+display();
