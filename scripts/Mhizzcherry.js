76b18f38ecba7b5d4f4465cd72b4e85909043468
Mhizzcherry <64986196+Mhizzcherry@users.noreply.github.com>
2020-06-06 12:20:41 +0100

    Update mhizzcherry
    changes successfully added

--- scripts/Mhizzcherry.js
+++ scripts/Mhizzcherry.js
@@ -4,5 +4,5 @@ var language="Javascript";
 var email="juliusonome2@gmail.com";
 
   console.log(
-    "Hello World, this is " + fullName + " with HNGi7 ID " + No + " using " + language + " for stage 2 task."+ email
+    "Hello World, this is " + fullName + " with HNGi7 ID " + No + " using " + language + "for stage 2 and "+ email
   );
