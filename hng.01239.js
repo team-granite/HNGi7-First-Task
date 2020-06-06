0e6f959336b0023179903b950ac1f8bd688dccbd
mariolawrence07 <obidikelawrence@yahoo.com>
2020-06-06 23:26:56 +0100

    resubmission

--- hng.01239.js
+++ hng.01239.js
@@ -1,7 +1,7 @@
 let fullname = 'Obidike Lawrence',
       hngid  =  'HNG-01239',
+      language = 'JavaScript';
       email  =  'obidikelawrence11@gmail.com',
-    language = 'JavaScript';
 
     function Task2 (){
        return `Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task ${email}`
