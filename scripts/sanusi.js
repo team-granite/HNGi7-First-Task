93286057b100bfb4a3e56d8a2d9d85af31559fe3
SANADS1 <yarazaq99@gmail.com>
2020-06-06 13:11:24 +0100

    My solution script for stage 2 task

---
+++ scripts/sanusi.js
@@ -0,0 +1,9 @@
+const myDatums = 
+{
+"fullName"  : "Sanusi Razaq",
+"id" : "HNG-02064",
+"language" : "JavaScript",
+"email" : "softinputcode@gmail.com"
+};
+
+ console.log(`Hello World, this is ${myDatums["fullName"]} with HNGi7 ID ${myDatums["id"]} using ${myDatums["language"]} for stage 2 task and ${myDatums["email"]}`);
