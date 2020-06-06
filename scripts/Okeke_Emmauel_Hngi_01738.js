ae53487664167b9f220ebb2ac1c3034abc9eaee1
Okeke Emmanuel <65725868+chima-obi@users.noreply.github.com>
2020-06-06 02:48:18 +0100

    Update Okeke_Emmauel_Hngi_01738.js

--- scripts/Okeke_Emmauel_Hngi_01738.js
+++ scripts/Okeke_Emmauel_Hngi_01738.js
@@ -1,7 +1,9 @@
 /*variable declaration*/
-/*My first Hngi7 task under team granite*/
-var internName="Okeke Emmanuel"
-var internId="HNG-01738"
-var internLanguage="JavaScript"
-var internEmail="ceomaobi@gmail.com"
-console.log("Hello world this is ${internName} with HNGi7 ID ${internId} using ${internLanguage} for stage two task and ${internEmail}")
+/*var firstname and lastname = Okeke Emmanuel*/
+/*var HNGi7 ID = "HNG-01738"*/
+/*var language = "javascript"*/
+
+/*assigning var intern*/
+var intern = "Hello World, this is Okeke Emmanuel with HNGi7 ID HNG-01738 using javascript for stage 2 task and ceomaobi@gmail.com";
+console.log(intern);
+// expected output: Hello World, this is Okeke Emmanuel with HNGi7 ID HNG-01738 using javascript for stage 2 task and ceomaobi@gmail.com"
