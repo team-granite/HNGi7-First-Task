bad7bace860a2759463e019f99663c6aaf7b7999
sbpythonista <asaajulekan@gmail.com>
2020-06-06 10:10:57 +0100

    smartbrain-HNG-05813

--- scripts/smartbrain-HNG-05813.py
+++ scripts/smartbrain-HNG-05813.py
@@ -7,7 +7,7 @@ def python_script():
     stage = "stage 2 task"
     statement = "Hello World,"
     e_mail = "asaajulekan@gmail.com"
-    return statement + " This is " + full_name +  " with HNGi7 ID HNG-" + hng_Id + " and email " + e_mail+ " using " + lng + " for " + stage
+    return statement + " This is " + full_name +  " with HNGi7 ID HNG-" + hng_Id + " and email " + e_mail + " using " + lng + " for " + stage
 
 
 result = python_script()
