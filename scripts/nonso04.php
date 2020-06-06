ad6528a34893338e70d04a14727a17d6081759bd
ChinonsoIke <@chinonsoikewelugo@gmail.com>
2020-06-07 00:43:48 +0100

    nonso task correction

--- scripts/nonso04.php
+++ scripts/nonso04.php
@@ -4,6 +4,6 @@
     $hng_id= "HNG-05917";
     $language= "PHP";
 
-    echo "Hello World, this is $name with HNGi7 id $hng_id using $language for stage 2 task and $email";
+    echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
     
 ?>
