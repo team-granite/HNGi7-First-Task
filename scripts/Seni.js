b07df9d40241cd4fa5330ac6044575ebe0f5b4ae
[Notanewbiecoder] <[oladokspelz@gmail,com]>
2020-06-06 06:31:57 -0700

    Seni's Script

---
+++ scripts/Seni.js
@@ -0,0 +1,13 @@
+let Name = 'Oladokun Seni';
+let myid = 'HNG-04250';
+let lang = 'Javascript';
+let email = "senioladokun44@gmail.com";
+
+console.log('Hello World, this is ' + 
+Name + 
+' with HNGi7 ID ' +
+ myid + 
+' using ' +
+lang + 
+' for stage 2 task. my email is ' + 
+email);
