fc9b48f6fe76f407c07f71e92764e23d82aa7db9
asmart59 <yhabeeb95@gmail.com>
2020-06-06 04:02:47 +0100

    asamrt59 first task script

---
+++ scripts/HabeebAY.py
@@ -0,0 +1,2 @@
+fullname, HNGi7_ID, language, email = "Habeeb Yusuf", "HNG-05243", "Python", "yhabeeb95@gmail.com"
+print(f'Hello World, this is {fullname} with HNGi7 ID {HNGi7_ID} using {language} for stage 2 task and {email}.')
