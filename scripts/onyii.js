4cd4bc678c55e3b035cbf4d8456a56fc31faa0de
Vchuks <64593598+Vchuks@users.noreply.github.com>
2020-06-06 20:16:11 +0100

    onyii task

--- scripts/onyii.js
+++ scripts/onyii.js
@@ -1,10 +1,5 @@
-<script type="text/javascript">
-
 var name="Chukwumee Onyinye";
 var num="HNG-04054";
 var lang="Javascript";
 var email="vchuks4all@yahoo.com";
-
 console.log("Hello World, this is "+name+" with HNGi7 ID "+num+" using "+lang+" for stage 2 task and "+email);
-
-</script>
