c9851042d12fd64478be0b8a0a6957b42fed867b
chibuzorihueze <59567733+chibuzorihueze@users.noreply.github.com>
2020-06-06 08:33:17 +0100

    Chibuzoe-Christian-Ihueze.php added

---
+++ scripts/Chibuzor-Christian-Ihueze.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = "Chibuzor Christian Ihueze";
+$id = "HNG-03829";
+$language = "PHP";
+$email = "krisykord@gmail.com";
+
+print(" Hello World, this is {$name} with HNGi7 ID {$id} and email {$email} using {$language} for stage 2 task ");
