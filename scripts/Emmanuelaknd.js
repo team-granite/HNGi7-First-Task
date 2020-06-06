a1b95b59a62b2c2701d5c1c5456e9a1f183e7714
Akande Emmanuel <55928467+sikochapo@users.noreply.github.com>
2020-06-06 15:53:26 +0100

    Update Emmanuelaknd.js

--- scripts/Emmanuelaknd.js
+++ scripts/Emmanuelaknd.js
@@ -4,4 +4,4 @@ var email = "akandeemmanuel1993@gmail.com";
 var language = "JavaScript";
 
 
-console.log ("Hello World, this is " + fullname + " with HNGi7 ID " + id +  " using " + language + " for stage 2 task and " + email);
+console.log ("Hello World, this is " + fullname + " with HNGi7 ID " + id + " using " + language + " for stage 2 task and " + email);
