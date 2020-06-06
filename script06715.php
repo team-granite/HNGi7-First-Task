e7461c4c0f7641b77d09b513aefeac8a3fd57f2d
Taymie-tee <46806277+Taymie-tee@users.noreply.github.com>
2020-06-06 08:52:13 +0100

    Update script06715.php

--- script06715.php
+++ script06715.php
@@ -1,7 +1,7 @@
 <?php
 	function intro($name, $hng_id, $language, $email){
 
-		$output = "Hello World, this is ".$name. "with HNGi7 ID: "$hng_id. "using ".$language. "for stage 2 task and".$email;
+		$output = "Hello World, this is ".$name. "with HNGi7 ID: "$hng_id. "using ".$language. "for stage 2 task and ".$email;
 		return $output;
 
 	} echo intro ("AKINTOYE TEMITOPE", "HNG-06715", "PHP", "benedictakintoye@gmail.com");
