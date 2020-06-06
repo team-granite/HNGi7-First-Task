d81382e80ccde56ce9b5101371526e81ac7f032c
Nk-hash <66283045+Nk-hash@users.noreply.github.com>
2020-06-06 14:32:42 +0100

    my stage 1 task

---
+++ scripts/Nsikak.js
@@ -0,0 +1,15 @@
+const profile = {
+  name: "Nsikak Sunday",
+  id: "HNG-03323",
+  language: "Javascript",
+  email: "freetrialtwo19@gmail.com",
+};
+
+console.log(
+  "Hello World, this is " +
+    profile["name"] +
+    " with HNGi7 ID " +
+    profile["id"] +
+    " using " +
+    profile["language"]
+);
