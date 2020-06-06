b4fe90a598db4f3f2c67ff9f66ae89322df7aa63
walexicoxyborg <jamiuwale20@gmail.com>
2020-06-06 09:54:43 +0100

    Create JAMIU Abdulhafeez HNG-02008 - Copy.py

---
+++ scripts/JAMIU Abdulhafeez HNG-02008 - Copy.py
@@ -0,0 +1,4 @@
+content = "Hello World, I am JAMIU Abdulhafeez with HNGi7 ID HNG-02008 using python for stage 2 task and my email is jamiuwale20@gmail.com"
+def script():
+    print(content)
+script()
