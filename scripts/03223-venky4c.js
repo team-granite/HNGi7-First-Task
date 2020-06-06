23bb7993aa136a0f877933c08f0dbda7195dcbb0
venky4c <venky4c@gmail.com>
2020-06-06 15:02:03 +0530

    venky4c tested script successfully

---
+++ scripts/03223-venky4c.js
@@ -0,0 +1,8 @@
+const intern_name = "venky4c";
+const intern_email = "venky4c@gmail.com";
+const intern_Id = "HNG-03223";
+const intern_language = "Javascript";
+
+( () => {
+    console.log(`Hello World, this is ${intern_name} with HNGi7-ID ${intern_Id}, using ${intern_language} for stage 2 task, and my email is ${intern_email}`);
+})()
