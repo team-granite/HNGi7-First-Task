48204260fb1b47f225db36f6b42b050cdaf02747
quervolvh <quervolvh@gmail.com>
2020-06-07 03:18:44 +0100

    Update and rename enumah-nneka-karen.php to enumah-nneka-karen.js

---
+++ scripts/enumah-nneka-karen.js
@@ -0,0 +1,8 @@
+output_my_details("Nneka Karen", "220202029920", 'Javascript', 'karenenumah@gmail.com');
+  
+
+function output_my_details(name,id, language, email) {
+
+console.log(`Hello World, this is ${name} with HNGi7 ID HNG-${id} using ${language} for stage 2 task and ${email}`);
+ 
+}
