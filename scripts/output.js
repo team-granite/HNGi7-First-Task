da66de7e4f0ec6db84e4d99331a6582196a64463
Okolie-Samuel <66499386+Okolie-Samuel@users.noreply.github.com>
2020-06-07 17:23:10 +0100

    Add files via upload

---
+++ scripts/output.js
@@ -0,0 +1,9 @@
+﻿const name = "Okolie Smauel";
+const id = "HNG-06795";
+const lang = "Javascript";
+const email = "uche7877@gmail.com";
+function outPut(){
+    return 'Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' +
+    lang + ' for stage 2 task and ' + email
+}
+console.log(outPut());
