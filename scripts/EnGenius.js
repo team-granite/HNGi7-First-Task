75310c465d26e2607c06ab107d3fe4756e03efa4
EnGeniuss <64276263+EnGeniuss@users.noreply.github.com>
2020-06-07 10:38:50 +0100

    Update EnGenius.js

--- scripts/EnGenius.js
+++ scripts/EnGenius.js
@@ -1,5 +1,5 @@
 var language='Javascript';
 var Name='Muhammed Yakub';
-var ID='HNG06594';
+var ID='HNG-06594';
 var email='engeniusss@gmail.com';
 console.log('Hello World, this is' + Name + 'with HNGi7 ID' + ID + 'using' + language + 'for stage 2 task and' + email);
