c21aef0e3314f5406231945392cdcfd7bad1a520
janilawy <janilawy@gmail.com>
2020-06-07 02:02:27 +0100

    My assignment on HNG Task

---
+++ scripts/janilawy.js
@@ -0,0 +1,9 @@
+let data = {
+    fullName: "Tijani Muhammed lawal",
+    id: "HNGi7 ID HNG-03379",
+    language: "javaScript",
+}
+
+let = {fullName, id, language} = data;
+let text = `Hello World, this is ${fullName} with ${id} using ${language} for stage 2 task`
+console.log(text);
